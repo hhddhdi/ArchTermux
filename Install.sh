ab4ab7bdb61e017e12e37d063acd7eea7cfc0336
bird <77020576+hhddhdi@users.noreply.github.com>
2025-04-20 14:30:21 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,7 +1,7 @@
 echo "Updating..."
-
 pkg update -y > /dev/null 2>&1
 pkg upgrade -y > /dev/null 2>&1
+echo "installing packages..."
 pkg install x11-repo -y > /dev/null 2>&1
 pkg install termux-x11-nightly -y > /dev/null 2>&1
 pkg install tur-repo -y > /dev/null 2>&1
@@ -17,8 +17,8 @@ pkg update -y > /dev/null 2>&1
 pkg install tsu -y > /dev/null 2>&1
 pkg install pulseaudio -y > /dev/null 2>&1
 
-echo "installing packages..."
-pkg install pulseaudio -y > /dev/null 2>&1
+
+
 clear
 echo "Run the command './install.sh'"
 su -c 'curl -o install.sh https://raw.githubusercontent.com/hhddhdi/ArchTermux/main/installsu.sh' > /dev/null 2>&1
